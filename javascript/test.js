dc35e56d98fa3450c7d61bee6c4a792d6b479ad6
Cole Stoltz <stoltcol2000@gmail.com>
2022-06-25 18:03:50 -0500

    more js testing and some editing done to the html file paths

--- javascript/test.js
+++ javascript/test.js
@@ -1,2 +1,10 @@
-var x = 100;
-document.write(x);
+function person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+var per = new person("lemon", 12);
+
+var arr = [12, 1, 43];
+
+console.log(person.length);
